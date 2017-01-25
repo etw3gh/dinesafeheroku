1d7e5b58eb93c10420a39b4d2c1e3626475d2696
Eli Tabello <etabw3@gmail.com>
2017-01-25 12:42:53 -0500

    'find query forgot'

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/inspections/:venue_id' => 'inspections#get'
   get '/find/:term' => 'inspections#find'
-
+  get '/near/:lat/:lng/:limit' => 'inspections#near'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
