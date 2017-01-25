ec8fa6b810b1098fee7f42f39b350f9173b1f7c3
Eli Tabello <etabw3@gmail.com>
2017-01-25 12:41:05 -0500

    'find query'

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -21,6 +21,19 @@ class InspectionsController < ApplicationController
     render :json => json_result
   end
 
+  def near
+    lat = params[:lat]
+    lng = params[:lng]
+    limit = params[:limit]
+
+    # http://stackoverflow.com/questions/2234204/latitude-longitude-find-nearest-latitude-longitude-complex-sql-or-complex-calc
+    distance_clause = "(6371 * acos(cos(radians(#{lat})) * cos(radians(lat)) * cos(radians(lng) - radians(#{lng})) + sin(radians(#{lat})) * sin(radians(lat )))) AS distance"
+    nearby_query = "SELECT a.streetname, a.num, #{distance_clause} FROM addresses a INNER JOIN venues v ON v.address_id=a.id ORDER BY distance ASC LIMIT #{limit}"
+    results = ActiveRecord::Base.connection.execute(nearby_query)
+    render :json => {'result': results, 'count': results.count, 'query': {'lat': lat, 'lng': lng, 'limit': limit}}
+
+  end
+
 
   def find
     term = params[:term]
