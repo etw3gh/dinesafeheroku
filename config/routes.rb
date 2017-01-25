89a9e9931aed47978100273219e8698bdb7321e9
Eli Tabello <etabw3@gmail.com>
2017-01-25 11:53:28 -0500

    'find query'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/inspections/:venue_id', to: 'inspections#get'
+  get '/inspections/:venue_id' => 'inspections#get'
+  get '/find/:term' => 'inspections#find'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
