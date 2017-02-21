6427537a4380121659bd621da34e424c8d477968
Eli Tabello <etabw3@gmail.com>
2017-02-21 18:58:56 -0500

    'numbers by street and street by municipality'

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,6 +1,22 @@
 class AddressesController < ApplicationController
-  def mun
-    render :json => Address.select(:mun).distinct.pluck(:mun)
 
+  # retuns the municipalities in alpha order
+  def mun
+    render :json => Address.order(:mun).distinct.pluck(:mun)
   end 
+
+  def munstreets
+    mun = params[:mun]
+    render :json => Address.where(:mun=>mun).order(:streetname).distinct.pluck(:streetname)
+  end
+  # returns all streets in the city in alpha order
+  def streets
+    render :json => Address.order(:streetname).distinct.pluck(:streetname)
+  end
+
+  # returns just the numbers in ascending order for a given street
+  def numbers
+    street = params[:street].downcase
+    render :json => Address.where(:streetname=>street).order(:num).pluck(:num)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do |map|
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/mun' => 'addresses#mun'
+  get '/streets' => 'addresses#streets'
+  get '/numbers'=> 'addresses#numbers'
   
   root 'welcome#index'
    
