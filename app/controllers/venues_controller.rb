a316f52c187fe6152bfb7abdc87b0fa12d4d1fb7
Eli Tabello <etabw3@gmail.com>
2017-02-21 14:32:00 -0500

    'pho'

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -7,4 +7,20 @@ class VenuesController < ApplicationController
   def index
     @venues = Venue.all
   end
+  def pho
+    result = []
+    venues =  Venue.where("venuename like '% pho %' or venuename like 'pho %'")
+    venues.each do |v|
+      a = Address.where(:id => venue.address_id)
+      item = {}
+      item['address'] = "#{a.num} #{a.streetname}"
+      item['lat'] = a.lat
+      item['lng'] = a.lng
+      item['mun'] = a.mun
+      item['name'] = v.venuename
+      item['vid'] = v.id
+      result.push(item)
+    end
+    render :json => result
+  end
 end
