263089512f443fabbd03ef4c7bd1e0fa78ac59cc
Eli Tabello <etabw3@gmail.com>
2017-02-21 18:10:01 -0500

    'cast to downtown in query'

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
     "(#{km_factor} * acos(cos(radians(#{lat})) * cos(radians(lat)) * cos(radians(lng) - radians(#{lng})) + sin(radians(#{lat})) * sin(radians(lat )))) AS distance "
   end  
   def geoloc(lat, lng, limit, where='')
-    nearby_query = "SELECT v.venuename as name, v.id, a.mun, a.lat, a.lng, a.num, a.num  || ' ' || a.streetname as address, #{distance_clause(lat,lng)} FROM addresses a INNER JOIN venues v ON v.address_id=a.id #{where} ORDER BY distance ASC LIMIT #{limit}"
+    nearby_query = "SELECT v.venuename as name, v.id, replace(a.mun, 'former Toronto', 'Downtown') as mun, a.lat, a.lng, a.num, a.num  || ' ' || a.streetname as address, #{distance_clause(lat,lng)} FROM addresses a INNER JOIN venues v ON v.address_id=a.id #{where} ORDER BY distance ASC LIMIT #{limit}"
     ActiveRecord::Base.connection.execute(nearby_query)
   end  
 end
