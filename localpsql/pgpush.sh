de3dbddede5d1f2f8a1e9e4cea20231cadf49dc5
Eli Tabello <etabw3@gmail.com>
2017-03-13 18:58:22 -0400

    pgpush.sh add pg user and pwd ENV var

--- localpsql/pgpush.sh
+++ localpsql/pgpush.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-heroku pg:push dinesafe_development postgresql-opaque-27045 --app dinesafe
+PGUSER=ds PGPASSWORD=$DINESAFE_DATABASE_PASSWORD heroku pg:push dinesafe_development postgresql-opaque-27045 --app dinesafe
