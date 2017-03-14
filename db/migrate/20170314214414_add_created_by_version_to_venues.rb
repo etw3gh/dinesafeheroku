0cbe1591ac70b8a6f6b57b45a855de8ef7b4d19b
Eli Tabello <etabw3@gmail.com>
2017-03-14 17:56:40 -0400

    add_column integer

--- db/migrate/20170314214414_add_created_by_version_to_venues.rb
+++ db/migrate/20170314214414_add_created_by_version_to_venues.rb
@@ -1,5 +1,5 @@
 class AddCreatedByVersionToVenues < ActiveRecord::Migration[5.0]
   def change
-    add_column :venues, :createdbyversion
+    add_column :venues, :createdbyversion, :integer
   end
 end
