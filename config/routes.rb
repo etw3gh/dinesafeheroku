f400552465a9d2454fe541e45f76a960b4023ba9
Eli Tabello <etabw3@gmail.com>
2017-03-30 00:48:47 -0400

    'domain constraint'

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,43 @@
 Rails.application.routes.draw do |map|
-  
-  # a Query from populated dropdowns will have exact values
-  get '/inspections' => 'inspections#get'
-  get '/statuses' => 'inspections#statuses'
+  constraints AppDomain do
+    # a Query from populated dropdowns will have exact values
+    get '/inspections' => 'inspections#get'
+    get '/statuses' => 'inspections#statuses'
 
-  # get nearby from dropdown
-  # give street name, then populate street numbers
-  # produce lat, lng for use in route
-  # gives a list of nearby venues
-  get '/byadddress' => 'inspections#byadddress'
+    # get nearby from dropdown
+    # give street name, then populate street numbers
+    # produce lat, lng for use in route
+    # gives a list of nearby venues
+    get '/byadddress' => 'inspections#byadddress'
 
-  get '/pho' => 'venues#phoall'
+    get '/pho' => 'venues#phoall'
 
-  get '/phoby' => 'venues#nearby'
-  
-  get '/pho/:lat/:lng/:lim' => 'venues#pho', :constraints => {:lat => /\-?\d+(.\d+)?/, :lng => /\-?\d+(.\d+)?/, :lim => /\d+/}
+    get '/phoby' => 'venues#nearby'
+    
+    get '/pho/:lat/:lng/:lim' => 'venues#pho', :constraints => {:lat => /\-?\d+(.\d+)?/, :lng => /\-?\d+(.\d+)?/, :lim => /\d+/}
 
-  get '/find/:term' => 'inspections#find'
-  get '/near/' => 'inspections#near'
-  get '/nearsearch' => 'inspections#nearsearch'
+    get '/find/:term' => 'inspections#find'
+    get '/near/' => 'inspections#near'
+    get '/nearsearch' => 'inspections#nearsearch'
 
-  get '/test/:a/:b' => 'venues#test'
+    get '/test/:a/:b' => 'venues#test'
 
-  get '/byaddr/:num/:street/:numvariance/:limit' => 'inspections#byaddr', :defaults => {:numvariance=>10, :limit=>20}
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/munstreets' => 'addresses#munstreets' 
-  get '/mun' => 'addresses#mun'
-  get '/streets' => 'addresses#streets'
-  get '/numbers'=> 'addresses#numbers'
-  get '/venue/:vid' => 'venues#get'
-  get '/venues' => 'venues#all'
-  get '/ping' => 'welcome#ping'
+    get '/byaddr/:num/:street/:numvariance/:limit' => 'inspections#byaddr', :defaults => {:numvariance=>10, :limit=>20}
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    get '/munstreets' => 'addresses#munstreets' 
+    get '/mun' => 'addresses#mun'
+    get '/streets' => 'addresses#streets'
+    get '/numbers'=> 'addresses#numbers'
+    get '/venue/:vid' => 'venues#get'
+    get '/venues' => 'venues#all'
+    get '/ping' => 'welcome#ping'
 
-  root 'welcome#index'
+    root 'welcome#index'
+  end
 
-
+end
+class AppDomain
+  def self.matches(request)
+    request.subdomain == 'dinesafe' && request.domain == 'herokuapp.com'
+  end
 end
