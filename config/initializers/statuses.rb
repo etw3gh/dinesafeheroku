a58d1945528eaf6665927ef11d1cb3058a6a9bd2
Eli Tabello <etabw3@gmail.com>
2017-04-02 15:48:50 -0400

    'add all to valid status'

--- config/initializers/statuses.rb
+++ config/initializers/statuses.rb
@@ -1,4 +1,4 @@
 # Instead of calling the DB every time a route is accessed,
 # Store the statuses array in a variable
 
-Rails.application.config.statuses = Inspection.order(:status).distinct.pluck(:status)
+Rails.application.config.statuses = Inspection.order(:status).distinct.pluck(:status).push('all')
