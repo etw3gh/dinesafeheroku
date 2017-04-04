095117396bf707ddac398999ed6fbc131da202ae
Eli Tabello <etabw3@gmail.com>
2017-04-04 15:56:33 -0400

    'put back string constraint'

--- config/constraints/string_constraints.rb
+++ config/constraints/string_constraints.rb
@@ -1,9 +1,10 @@
 module StringContraints
   def self.matches? request
     puts YAML::dump(object)
-    if request.street.nil?
-      return true
-    end
-    request.street.length < 5
+    return true
+    # if request.street.nil?
+    #   return true
+    # end
+    # request.street.length < 5
   end
 end
