3af941a26a2b3e26f89a8c41939f9bbec291a609
Eli Tabello <etabw3@gmail.com>
2017-04-05 16:16:59 -0400

    'x-api-key for curl req'

--- config/constraints/request_constraints.rb
+++ config/constraints/request_constraints.rb
@@ -3,10 +3,12 @@ module Restrictions
     # if referer is null, then check for IP on whitelist (home)
     # otherwise, restrict to domains used by web apps on whitelist 
     r = request.referer
-    puts "***** referer: #{r}"
+    
+    api_key = request.headers['X-Api-Key']
+
     if r.nil?
-      puts "*****no req.referer: ip: #{request.remote_ip}, home_ip: #{Rails.configuration.home_ip}" 
-      return request.remote_ip == Rails.configuration.home_ip
+      puts "*****no req.referer: ip: #{api_key}, home_ip: #{Rails.configuration.api_key}" 
+      return api_key == Rails.configuration.api_key
     else
       white_list = Rails.configuration.white_list
       return r.in?(white_list)
