a5b535dcf101e3500a8c5ad61ac56900bbd06dc8
Eli Tabello <etabw3@gmail.com>
2017-04-05 18:58:29 -0400

    'no route constraint for alpha. api is secure. sanitize input before sql ops'

--- config/constraints/segment_regex.rb
+++ config/constraints/segment_regex.rb
@@ -7,15 +7,12 @@ class SegmentRegex
   # regex that matches to an int
   re_int =  /\d+/
 
-  re_alpha_num = /(?![^A-Za-z0-9 ]+)/
-
   @@segments = {
       :lat => re_lat_lng, 
       :lng => re_lat_lng, 
       :lim => re_int,
       :var => re_int,
       :vid => re_int,
-      :search => re_alpha_num,
       :status => Regexp.union(Rails.application.config.statuses)
   }
 
