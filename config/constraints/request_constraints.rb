f246a7b42b3699bf146c9da022409706d9f4d021
Eli Tabello <etabw3@gmail.com>
2017-04-06 13:48:44 -0400

    'comments'

--- config/constraints/request_constraints.rb
+++ config/constraints/request_constraints.rb
@@ -1,13 +1,21 @@
+# Locks use of this rails app to certain front end applications as defined by an environment variable
+#
+# Testing:
+# For CURL requests, an api key must be specified (can be obtained by ENV var)
+# Using a browser to view JSON response has been disabled,
+# use Postman (https://www.getpostman.com/) for chrome or firefox
+# set X-Api-Key in the headers section
+#
 module Restrictions
   def self.matches? request
-    # if referer is null, then check for IP on whitelist (home)
+    # 
+    # if referer is null, then check for API KEY  
     # otherwise, restrict to domains used by web apps on whitelist 
     r = request.referer
     
     api_key = request.headers['X-Api-Key']
 
     if r.nil?
-      puts "*****no req.referer: ip: #{api_key}, home_ip: #{Rails.configuration.api_key}" 
       return api_key == Rails.configuration.api_key
     else
       white_list = Rails.configuration.white_list
