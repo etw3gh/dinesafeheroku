cf88c244a145855c3370aa72a343ff24f49920c9
Eli Tabello <etabw3@gmail.com>
2017-04-10 13:54:28 -0400

    'mongo'

--- config/initializers/mongo_client.rb
+++ config/initializers/mongo_client.rb
@@ -1,4 +1,14 @@
 class MONGODB
   cattr_accessor :client
-   
+  def client
+    host = "#{ENV['OC_MONGO_IP']}:#{ENV['OC_MONGO_PORT']}"
+    u = ENV['OC_MONGO_USER']
+    p = ENV['OC_MONGO_PASSWORD']
+    admin = ENV['OC_MONGO_DB']
+    c = ENV['OC_MONGO_COLLECTION']
+    db = ENV['OC_MONGO_DS_DB']
+    client = Mongo::Client.new([ host ], :database=>admin, :user=>u, :password=>p)
+    db = client.database
+    db.collection_names
+  end 
 end
