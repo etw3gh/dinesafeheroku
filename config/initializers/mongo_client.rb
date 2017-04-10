989bb2d6094f7e6fcdbc84929b679eeb31137f54
Eli Tabello <etabw3@gmail.com>
2017-04-10 14:59:27 -0400

    'try switch db'

--- config/initializers/mongo_client.rb
+++ config/initializers/mongo_client.rb
@@ -3,12 +3,11 @@ class MONGODB
     host = "#{ENV['OC_MONGO_IP']}:#{ENV['OC_MONGO_PORT']}"
     u = ENV['OC_MONGO_USER']
     p = ENV['OC_MONGO_PASSWORD']
-    admin = ENV['OC_MONGO_DB']
+    db = ENV['OC_MONGO_DB']
     c = ENV['OC_MONGO_COLLECTION']
-    db = ENV['OC_MONGO_DS_DB']
-    client = Mongo::Client.new([ host ], :database=>db, :user=>u, :password=>p)
-    client()
-    db = client.database
+   
+    client = Mongo::Client.new([ host ], :database=>admin, :user=>u, :password=>p)
+    db = client.database = db
     db.collection_names
   end 
 end
