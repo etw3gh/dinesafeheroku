8a1eefd8f9757355c16709426726116feadb7f56
Eli Tabello <etabw3@gmail.com>
2017-04-11 15:15:52 -0400

    'more info'

--- app/controllers/mongo_controller.rb
+++ app/controllers/mongo_controller.rb
@@ -1,9 +1,12 @@
 class MongoController < ApplicationController
   def collections
     dsadmin_count = MONGODB.dsadmin.count
-    render :json => { 
-      MONGODB.collections, 
-      'dsadmin': {count: dsadmin_count} 
+    render :json => {
+      'collections': MONGODB.collections, 
+      'dsadmin': {
+        db: MONGODB.ds,
+        count: dsadmin_count
+      } 
     }
   end
 end
