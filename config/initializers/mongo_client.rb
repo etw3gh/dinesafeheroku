106e38542ea04846bc3052c31284195b6848df32
Eli Tabello <etabw3@gmail.com>
2017-04-10 21:11:27 -0400

    'try to switch to dinesafe db'

--- config/initializers/mongo_client.rb
+++ config/initializers/mongo_client.rb
@@ -7,7 +7,7 @@ class MONGODB
     c = ENV['OC_MONGO_COLLECTION']
     ds = ENV['OC_MONGO_DS']
     client = Mongo::Client.new([ host ], :database=>db, :user=>u, :password=>p)
-    #client.use(ds)
+    #
     db = client.database
     dbs = client.database_names
     dbs.each_with_index do |d, i|
@@ -16,6 +16,16 @@ class MONGODB
     db.collection_names.each_with_index do |c, i|
       puts "#{i} collection: #{c}"
     end
+
+    client.use('dinesafe')
+    dbs = client.database_names
+    dbs.each_with_index do |d, i|
+      puts "#{i} db: #{d}"
+    end
+    db.collection_names.each_with_index do |c, i|
+      puts "#{i} collection: #{c}"
+    end
+
     {collections: db.collection_names}
   end 
 end
