204195dfcfa75f636df212a69734f7f332f5af4b
Eli Tabello <etabw3@gmail.com>
2017-04-11 14:43:38 -0400

    'close obj'

--- app/controllers/mongo_controller.rb
+++ app/controllers/mongo_controller.rb
@@ -1,6 +1,9 @@
 class MongoController < ApplicationController
   def collections
     dsadmin_count = MONGODB.dsadmin.count
-    render :json => {MONGODB.collections, 'dsadmin': {count: dsadmin_count}
+    render :json => { 
+      MONGODB.collections, 
+      'dsadmin': {count: dsadmin_count} 
+    }
   end
 end
