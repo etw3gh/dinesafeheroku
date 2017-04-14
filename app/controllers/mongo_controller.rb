3b953624953d1707fc9937913c89833ce9638780
Eli Tabello <etabw3@gmail.com>
2017-04-14 13:27:23 -0400

    'dont get collection names from non admin db '

--- app/controllers/mongo_controller.rb
+++ app/controllers/mongo_controller.rb
@@ -2,7 +2,6 @@ class MongoController < ApplicationController
   def collections
     dsadmin_count = MONGODB.dsadmin.count
     render :json => {
-      'collections': MONGODB.collections, 
       'dsadmin': {
         db: MONGODB.ds,
         count: dsadmin_count
--- localmongo/mongo.sh
+++ localmongo/mongo.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 # connect to mongo instance hosted on an aws server
 # set $OC_MONGO_{USER|PWD|IP|PORT|DB} in .bashrc and Heroku settings
-mongo -u $OC_DS_USER -p $OC_MONGO_PWD $OC_MONGO_IP:$OC_MONGO_PORT/$OC_MONGO_DS
+
+if [ "$1"  = "--admin" ] || [ "$1"  = "-a" ]
+then
+  USER=$OC_MONGO_DB   
+  DB=$OC_MONGO_DB   
+else
+  USER=$OC_DS_USER   
+  DB=$OC_MONGO_DS
+fi
+
+mongo -u $USER -p $OC_MONGO_PWD $OC_MONGO_IP:$OC_MONGO_PORT/$DB
