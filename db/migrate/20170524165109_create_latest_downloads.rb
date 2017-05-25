eb4da98de9af50a5d1235d1b0581506e5d9fa6d2
Eli Tabello <etabw3@gmail.com>
2017-05-25 14:47:00 -0400

    latestdl migration

--- db/migrate/20170524165109_create_latest_downloads.rb
+++ db/migrate/20170524165109_create_latest_downloads.rb
@@ -1,6 +1,10 @@
 class CreateLatestDownloads < ActiveRecord::Migration[5.0]
   def change
     create_table :latest_downloads do |t|
+      t.integer :lastmodxml
+      t.integer :lastmodgeo
+      t.string  :md5xml
+      t.string  :md5geo
     end
   end
 end
