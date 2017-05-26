affe8b49613087fb97db4bfe52e4a965ddf194a5
Eli Tabello <etabw3@gmail.com>
2017-05-26 16:17:36 -0400

    test rufus

--- config/cron_tasks/sched_toronto.rb
+++ config/cron_tasks/sched_toronto.rb
@@ -11,8 +11,13 @@ cron_job = "20 #{cron_hour} * * 1-5"
 #           mm hh               m-f
 
 puts "Starting Cron task. Will check for a new dinesafe archive every weekday at #{cron_hour}pm"
+s = Rufus::Scheduler.new
 
-Rufus::Scheduler.new.cron cron_job do
+s.cron cron_job do
   puts ('rufus')
   system('rake sched:dl')
+end
+
+s.every '1d', first: :now do
+  puts "rufus now!!!!!!!!!!! but daily!!!!!!!!!!!"
 end
