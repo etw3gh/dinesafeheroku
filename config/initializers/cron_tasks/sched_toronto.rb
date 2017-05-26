6f546fa9f6eccb2a308c9d19225fd2249999541b
Eli Tabello <etabw3@gmail.com>
2017-05-26 16:28:20 -0400

    430 cron

--- config/initializers/cron_tasks/sched_toronto.rb
+++ config/initializers/cron_tasks/sched_toronto.rb
@@ -4,13 +4,12 @@
 # starts a 'cron' task as per the directives in cron_job
 # Rufus Scheduler will execute the code in its block at that time
 
-# set for noon as most archives seem to be done by late morning
 cron_hour = 16
+cron_min = 30
+cron_job = "#{cron_min} #{cron_hour} * * 1-5"
+#             mm          hh             m-f
 
-cron_job = "20 #{cron_hour} * * 1-5"
-#           mm hh               m-f
-
-puts "Starting Cron task. Will check for a new dinesafe archive every weekday at #{cron_hour}pm"
+puts "Starting Cron task. Will check for a new dinesafe archive every weekday at #{cron_hour}:#{cron_min}"
 s = Rufus::Scheduler.new
 
 s.cron cron_job do
