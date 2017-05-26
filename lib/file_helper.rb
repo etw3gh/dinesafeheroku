ef3c0fa7cb5a752259e204ea007af4c1f4ac4099
Eli Tabello <etabw3@gmail.com>
2017-05-26 18:44:23 -0400

    task jsonify:geo

--- lib/file_helper.rb
+++ lib/file_helper.rb
@@ -55,15 +55,15 @@ class FileHelper
     end
   end
 
-# https://stackoverflow.com/a/37195592/6826791
-def extract_zip(file, destination)
-  self.make_dir(destination)
+  # https://stackoverflow.com/a/37195592/6826791
+  def extract_zip(file, destination)
+    self.make_dir(destination)
 
-  Zip::File.open(file) do |zip_file|
-    zip_file.each do |f|
-      fpath = File.join(destination, f.name)
-      zip_file.extract(f, fpath) unless File.exist?(fpath)
+    Zip::File.open(file) do |zip_file|
+      zip_file.each do |f|
+        fpath = File.join(destination, f.name)
+        zip_file.extract(f, fpath) unless File.exist?(fpath)
+      end
     end
   end
-end
 end
---
+++ lib/tasks/jsonify.rake
@@ -0,0 +1,16 @@
+namespace :jsonify do
+  geo_acq = Acquisitions.instance.shapefiles
+
+  @geo_txt = geo_acq[:textfiles]
+  @geo_zip = geo_acq[:archives]
+  @geo_shp = geo_acq[:shapefiles]
+  @geo_fn  = geo_acq[:filename]
+  @FH = FileHelper.new
+
+  task :geo, [:timestamp] => :environment do
+    source = '{}{}/{}'.format(@geo_shp, :timestamp, @geo_fn)
+    py = 'python3 ../../python/shaperip.py {} {}'.format(source, @geo_txt)
+    system(py)
+  end
+
+end
