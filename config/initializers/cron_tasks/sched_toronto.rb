55d898a72cc6d350990b436caad06f2ee1b074f6
Eli Tabello <etabw3@gmail.com>
2017-05-28 15:38:29 -0400

    comments and clearer puts on server

--- config/initializers/cron_tasks/sched_toronto.rb
+++ config/initializers/cron_tasks/sched_toronto.rb
@@ -4,22 +4,24 @@
 # WARNING: not for heroku, the data will be pushed by pgpush after processing
  
 
+# DO NOT INVOKE for rake OR console
 # https://github.com/jmettraux/rufus-scheduler#so-rails
-
 # https://github.com/jmettraux/rufus-scheduler#avoid-scheduling-when-running-the-ruby-on-rails-console
+
 unless defined?(Rails::Console) || File.split($0).last == 'rake'
 
   s = Rufus::Scheduler.singleton
   s.every '5m' do
     system('rake sched:dl')
   end
+  
   s.every '1d', first: :now do
-    puts '*** Checking remote server with rufus scheluler....'
+    puts "\n\+++ *** Checking remote server with rufus scheluler +++ *** \n\n"
     #system('rake sched:dl')
   end
 
   s.every '2d', first: :now do
-    puts 'Backing up downloads directory to raid array...'
+    puts "\n+++ *** Backing up downloads directory to raid array +++ *** \n\n"
     #system('./backup.sh')
   end
 end
