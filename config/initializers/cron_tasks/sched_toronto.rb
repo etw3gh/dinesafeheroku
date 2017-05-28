9d6d56d933981422b0eb8abcf77c5737e174b5db
Eli Tabello <etabw3@gmail.com>
2017-05-28 14:53:18 -0400

    revert to sytem. moving on.

--- config/initializers/cron_tasks/sched_toronto.rb
+++ config/initializers/cron_tasks/sched_toronto.rb
@@ -1,20 +1,16 @@
-require 'rake/dsl_definition'
-
-
 # NOTE: A rails server must be running locally at all times 
 # `rails s &` for background operation 
 
 # WARNING: not for heroku, the data will be pushed by pgpush after processing
  
-
 s = Rufus::Scheduler.new
 
 s.every '1d', first: :now do
   puts '*** Checking remote server with rufus scheluler....'
-  Rake::Task('sched:dl').invoke
+  system('rake sched:dl')
 end
 
-s.every '2d', first: :now do
+s.every '2d' do
   puts 'Backing up downloads directory to raid array...'
   system('./backup.sh')
 end
