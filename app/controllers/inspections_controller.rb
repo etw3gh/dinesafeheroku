7f6071104f50e9c837f043446e1cb2b869633e12
Eli Tabello <etabw3@gmail.com>
2017-06-08 21:50:16 -0400

    restrict to latest version

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -65,7 +65,8 @@ class InspectionsController < ApplicationController
   
   def byvid
     vid = params[:vid]
-    inspections = Inspection.where(:venue_id => vid).order(:date=>:desc, :id=>:asc)
+    maxversion = Inspection.maximum(:version)
+    inspections = Inspection.where(:venue_id => vid, :version => max).order(:date=>:desc, :id=>:asc)
     render :json => inspections
   end
 
