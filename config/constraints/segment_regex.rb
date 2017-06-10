79c01a72d4541475ae1021481c37b658bfa20602
Eli Tabello <etabw3@gmail.com>
2017-06-10 03:20:06 -0400

    enable statuses

--- config/constraints/segment_regex.rb
+++ config/constraints/segment_regex.rb
@@ -6,14 +6,14 @@ class SegmentRegex
 
   # regex that matches to an int
   re_int =  /\d+/
-  #statuses = Inspection.distinct.pluck(:status).push('all')
+  statuses = Inspection.distinct.pluck(:status).push('all')
   @@segments = {
       :lat => re_lat_lng,
       :lng => re_lat_lng,
       :lim => re_int,
       :var => re_int,
       :vid => re_int,
-      #:status => Regexp.union(statuses)
+      :status => Regexp.union(statuses)
   }
 
 end
