7a300c35d68b0b21987f2335e89db91b703c0188
Eli Tabello <etabw3@gmail.com>
2017-06-09 22:59:14 -0400

    disable env

--- config/initializers/statuses.rb
+++ config/initializers/statuses.rb
@@ -1,4 +1,4 @@
 # Instead of calling the DB every time a route is accessed,
 # Store the statuses array in a variable
 # add type 'all' to retrieved array
-Rails.application.config.statuses = Inspection.distinct.pluck(:status).push('all')
+#Rails.application.config.statuses = Inspection.distinct.pluck(:status).push('all')
