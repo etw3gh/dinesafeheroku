670910adde192c86bfebab5d26e60a0729e949e1
Eli Tabello <etabw3@gmail.com>
2017-06-09 23:01:52 -0400

    rm statuses

--- config/initializers/statuses.rb
+++
@@ -1,4 +0,0 @@
-# Instead of calling the DB every time a route is accessed,
-# Store the statuses array in a variable
-# add type 'all' to retrieved array
-#Rails.application.config.statuses = Inspection.distinct.pluck(:status).push('all')
