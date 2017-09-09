8a4ffd9b0bb99140ba2b3710f2674b544b2818fd
Eli Tabello <etabw3@gmail.com>
2017-09-09 08:53:27 -0400

    'puts toks'

--- config/initializers/yelp_init.rb
+++ config/initializers/yelp_init.rb
@@ -1,8 +1,16 @@
 require 'yelp'
 
+yelp_id = ENV['YELP_ID']
+yelp_sec = ENV['YELP_SECRET']
+yelp_tok = ENV['YELP_TOKEN']
+
+puts yelp_id
+puts yelp_sec
+puts yelp_tok
+
 Yelp.client.configure do |config|
-  config.consumer_key = ENV['YELP_ID']
-  config.consumer_secret = ENV['YELP_SECRET']
-  config.token = ENV['YELP_TOKEN']
+  config.consumer_key = yelp_id
+  config.consumer_secret = yelp_sec
+  config.token = yelp_tok
   #config.token_secret = ENV['']
 end
